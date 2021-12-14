5918341933a9209f33d8bda468180b753890cf6a
Radish-26 <70419733+Radish-26@users.noreply.github.com>
2021-12-14 08:02:34 -0500

    Update methodPracitice.java

--- methodPracitice.java
+++ methodPracitice.java
@@ -1,1 +1,27 @@
 
+public class LightV3 {
+  boolean isOn;
+  public LightV3(){
+ 
+   }
+  void turnOn() {
+    isOn = true;
+    System.out.println("Is the Light on? " + isOn);
+
+  }
+  void turnOff() {
+    isOn = false;
+    System.out.println("Is the Light on? " + isOn);
+  }
+}
+
+class Main {
+  public static void main(String[] args) {
+    // create objects
+    LightV3 led = new LightV3();
+    LightV3 christmasLights = new LightV3();
+    
+    led.turnOn();
+    christmasLights.turnOff();
+  }
+}
