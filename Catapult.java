7c0251c2ebbef1ae35ff235316b8a42228a890ef
Radish-26 <70419733+Radish-26@users.noreply.github.com>
2022-03-07 11:02:52 -0500

    Update Catapult.java

--- Catapult.java
+++ Catapult.java
@@ -29,3 +29,39 @@ public class CatapultTester
             System.out.printf("\n");
         }
     }
+    
+    
+    
+    
+public class Catapult
+{
+    private double velocity;
+    private double degrees;
+    private double distance;
+    public Catapult(double v, double deg)
+    {
+        velocity = v;
+        degrees = deg;
+    }
+    
+    public double getVelocity()
+    {
+        return velocity;
+    }
+    
+    public double getDegrees()
+    {
+        return degrees;
+    }
+    
+    public double getDistance()
+    {
+        return distance;
+    }
+    
+    public void calcDistance()
+    {
+        distance = (Math.pow(velocity, 2) * Math.sin( 2 * Math.toRadians(degrees)) / 9.8);
+    }
+    
+}
